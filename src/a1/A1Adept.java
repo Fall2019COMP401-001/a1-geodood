0008b5da792736bef1dc3482c9451208b7e7fc24
geodood <geodood@users.noreply.github.com>
2019-09-02 00:16:30 -0400

    first commit, nice

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -10,5 +10,79 @@ public class A1Adept {
 
 		// Your code follows here.
 		
+		int itemCount = scan.nextInt();
+		
+		String[] itemName = new String[itemCount];
+		double[] price = new double[itemCount];
+		
+		for (int i = 0; i < itemCount; i++) {
+			itemName[i] = scan.next();
+			price[i] = scan.nextDouble();
+		}
+		
+		// A1Novice
+		
+		int customerCount = scan.nextInt();
+		
+		String[] firstName = new String[customerCount];
+		String[] lastName = new String[customerCount];
+		int[] numberOfItems = new int[customerCount];
+		double[] totalCost = new double[customerCount];
+		
+		for (int i = 0; i < customerCount; i++) {
+			firstName[i] = scan.next(); 
+			lastName[i] = scan.next();
+			int n = scan.nextInt();
+			numberOfItems[i] = n;
+			
+			for (int j = 0; j < n; j++) {
+				int quantity = scan.nextInt();
+				String itemBought = scan.next();
+				for (int k = 0; k < itemCount; k++) {
+					if (itemBought.equals(itemName[k])) {
+						totalCost[i] += quantity * price[k];
+					}
+				}	
+			}
+		} 
+		
+		scan.close();
+		
+		String biggest = findBiggest(customerCount, firstName, lastName, totalCost);
+		String smallest = findSmallest(customerCount, firstName, lastName, totalCost);
+		String average = findAverage(customerCount, totalCost);
+		
+		System.out.println("Biggest: " +  biggest);
+		System.out.println("Smallest: " + smallest);
+		System.out.println("Average: " + average);
+	}
+		
+	static String findBiggest(int num, String[] first, String[] last, double[] amount) {
+		String big = first[0] + " " + last [0] + " (" + String.format("%.2f", amount[0]) +")";
+		for (int i = 1; i < num; i++ ) {
+			if (amount[i] > amount [i-1]) {
+				big = first [i] + "  " + last [i] + "  (" + String.format("%.2f", amount [i]) +")";
+			}
+		}
+		return big;
+	}
+	
+	static String findSmallest(int num, String[] first, String[] last, double[] amount) {
+		String small = first[0] + " " + last [0] + " (" + String.format("%.2f", amount[0]) +")";
+		for (int i = 1; i < num; i++ ) {
+			if (amount[i] < amount [i-1]) {
+				small = first [i] + "  " + last [i] + " (" + String.format("%.2f", amount [i]) +")";
+			}
+		}
+		return small;
+	}
+	
+	static String findAverage (int num, double[] amount) {
+		double average = 0;
+		for (int i =0; i < amount.length; i++) {
+			average += amount[i];
+		}
+		average = average / num ;
+		return String.format("%.2f", average);
 	}
 }
