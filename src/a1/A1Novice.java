91b83800f5f2146187364a9a99913886e726411e
geodood <geodood@users.noreply.github.com>
2019-09-01 22:21:42 -0400

    Novice first commit; no itemName array

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,34 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		int total = scan.nextInt();
+		
+		String[] firstName = new String[total];
+		String[] lastName = new String[total];
+		int[] numberOfItems = new int[total];
+		double[] totalCost = new double[total];
+		
+		for (int i = 0; i < total; i++) {
+			firstName[i] = scan.next(); 
+			lastName[i] = scan.next();
+			int n = scan.nextInt();
+			numberOfItems[i] = n;
+			
+			totalCost[i] = 0;
+			int j = 0;
+			while (j < n) {
+				int quantity = scan.nextInt();
+				String itemName = scan.next();
+				double price = scan.nextDouble();
+				totalCost[i] += quantity * price;		
+				j++;
+			}	
+		} 
+		
+		scan.close();
+		
+		for (int i = 0; i < total; i++) {
+			System.out.println(firstName[i].charAt(0) + ". "+ lastName[i] + ": " + totalCost[i]);
+		}
 	}
 }
